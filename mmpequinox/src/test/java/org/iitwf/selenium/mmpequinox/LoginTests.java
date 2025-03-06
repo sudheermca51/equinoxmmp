a5d25f5e322e7b5c159cf19f6012267ff589503c
Shweta Jha <Shwetajha030114@gmail.com>
2025-03-06 09:10:00 -0500

    Added extent report in LogIn Test file

--- mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/LoginTests.java
+++ mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/LoginTests.java
@@ -33,18 +33,17 @@ public class LoginTests extends FrameworkLibrary {
 	@Test(dataProvider="DP")
 	public void testLogin(String username,String password) throws Exception
 	{
-<<<<<<< HEAD
+
 		
 		extentTest = extentReports.createTest("############Validate LogIn Tests########");
-=======
->>>>>>> refs/heads/main
+
 		launchBrowser(prop.getProperty("patient_url")); 
 		MMPUtility mmpUtil = new MMPUtility(driver);
-<<<<<<< HEAD
+
 		
-=======
+
 		launchBrowser(prop.getProperty("patient_url"));
->>>>>>> refs/heads/main
+
 		mmpUtil.login(username,password);
 		
 		String actualText = driver.findElement(By.xpath("//h3[normalize-space()='Patient Portal']")).getText();
@@ -53,13 +52,8 @@ public class LoginTests extends FrameworkLibrary {
 	    extentTest.addScreenCaptureFromPath(screenshotPath,"Navigation_To_Home_Page");
 		String expectedText = "Patient Portal";
 		Assert.assertEquals(actualText, expectedText);
-<<<<<<< HEAD
-		}
-=======
-		
-		
->>>>>>> refs/heads/main
+
 		 
-	
+	}
 
 }
