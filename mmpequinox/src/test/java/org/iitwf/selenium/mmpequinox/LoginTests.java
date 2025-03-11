709518fcae7f40aaadbf7b159e039a9a3c9d352c
Ashwani <akmkak_2001@yahoo.com>
2025-03-11 12:27:10 -0400

    Cleaned up existing login Test and added a new Test to login with invalid Login credentials.

--- mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/LoginTests.java
+++ mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/LoginTests.java
@@ -1,24 +1,21 @@
 package org.iitwf.selenium.mmpequinox;
 
-import java.io.IOException;
+import java.time.Duration;
 
 import org.iitwf.mmp.pages.patientmodule.MMPUtility;
-import org.iitwf.selenium.lib.ExcelUtils;
 import org.iitwf.selenium.lib.FrameworkLibrary;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.aventstack.extentreports.ExtentTest;
 
 public class LoginTests extends FrameworkLibrary {
-//			id="username"
-//			id="password"
-//			name="submit"
-//			//h3[normalize-space()='Patient Portal']
-//
-//			String expectedText = "Patient Portal";
-	
-	@DataProvider(name="DP")
+
+	/*@DataProvider(name="DP")
 	public String[][] feedData() throws IOException
 	{
 		String inputData[][] = ExcelUtils.getCellData("mmpdata.xlsx");
@@ -38,6 +35,51 @@ public class LoginTests extends FrameworkLibrary {
 		launchBrowser(prop.getProperty("patient_url"));
 		
 		 
+	}*/
+	
+	private ExtentTest extentTest;
+	
+	@Test(priority=1)
+	public void testLogin()
+	{
+		extentTest = extentReports.createTest("######## Valid Login credentials ########"); 
+		
+		MMPUtility mmpUtil = new MMPUtility(driver);
+		extentTest.info("Launching Browser");
+		launchBrowser(prop.getProperty("patient_url"));
+		mmpUtil.login(prop.getProperty("patient_username"),prop.getProperty("patient_password"));
+		extentTest.info("Entered Valid Login Credentials");
+		String actualText = driver.findElement(By.xpath("//h3[normalize-space()='Patient Portal']")).getText();
+		extentTest.info("Getting actual text - " + " " + actualText);
+		String expectedText = "Patient Portal";
+		extentTest.info("Expected text - " + " " + expectedText);
+		SoftAssert sa = new SoftAssert();
+		sa.assertEquals(actualText, expectedText);
+		
+	}
+	
+	@Test(priority=2)
+	public void testInvalidLogin()
+	{
+		 
+		extentTest = extentReports.createTest("######## Invalid Login credentials ########");
+		
+		MMPUtility mmpUtil = new MMPUtility(driver);
+		extentTest.info("Launching Browser");
+		launchBrowser(prop.getProperty("patient_url"));
+		mmpUtil.login("patient_username","patient_password");
+		extentTest.info("Entered Invalid Login Credentials");
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  // Wait for up to 10 seconds
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		String actualText = alert.getText();
+		extentTest.info("Getting actual text - " + " " + actualText);
+		alert.accept();
+		String expectedText = "Wrong username and password.";
+		extentTest.info("Expected text - " + " " + expectedText);
+		SoftAssert sa = new SoftAssert();
+		sa.assertEquals(actualText.trim(), expectedText.trim());
+
+		
 	}
 
 }
