cd3536c8235a904ae6f91452b55d99320ed36b18
Shweta Jha <Shwetajha030114@gmail.com>
2025-03-11 13:54:46 -0400

    Added Search Symptom Page and Test file

---
+++ mmpequinox/src/main/java/org/iitwf/mmp/pages/patientmodule/SearchSymptomPage.java
@@ -0,0 +1,51 @@
+package org.iitwf.mmp.pages.patientmodule;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class SearchSymptomPage {
+	
+	protected WebDriver driver;
+	@FindBy (xpath="//input[@id='search']")
+	private WebElement searchBox;
+	@FindBy (xpath="//input[@name='submit']")
+	private WebElement searchBtn;
+	@FindBy (xpath="//table[@class='table']/tbody/tr/td")
+	private WebElement displayedSymp;
+	
+	public SearchSymptomPage(WebDriver driver) {
+		this.driver = driver;
+		if(!driver.getTitle().equals("search Symptoms")) {
+			throw new IllegalStateException("This is not Search Symptom Page," +
+		                                               "current page is: " + driver.getCurrentUrl());
+			
+		}
+		PageFactory.initElements(driver, this);
+	}
+	
+	public String searchSymptom(String symptom) {
+		searchBox.sendKeys(symptom);
+		searchBtn.click();
+	    return"";
+	}
+	
+    public String invalidsearchSymptom(String symptom) {
+    	
+    	searchBox.clear();
+		searchBox.sendKeys(symptom);
+		searchBtn.click();
+		try {
+		if(displayedSymp==null || !(displayedSymp.isDisplayed())) {
+			return "Failed to locate symptom";
+		}
+		}catch(Exception e) {
+			e.getMessage();
+		}
+		return"";
+		
+	}
+
+
+}
---
+++ mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/SearchSymptomTests.java
@@ -0,0 +1,46 @@
+package org.iitwf.selenium.mmpequinox;
+
+import java.io.IOException;
+
+import org.iitwf.mmp.pages.patientmodule.HomePage;
+import org.iitwf.mmp.pages.patientmodule.MMPUtility;
+import org.iitwf.mmp.pages.patientmodule.SearchSymptomPage;
+import org.iitwf.selenium.lib.FrameworkLibrary;
+import org.iitwf.selenium.lib.ScreenshotUtil;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.aventstack.extentreports.ExtentTest;
+
+public class SearchSymptomTests extends FrameworkLibrary{
+	private ExtentTest extentTest;
+	SearchSymptomPage srchsymptomobj = null;
+	@Parameters({"srchsymptom","invalidsrchsymptom"})
+	@Test
+	public void validateSearchSymptomBox(String srchsymptom,String invalidsrchsymptom) throws Exception  {
+		extentTest = extentReports.createTest("############Validate Search Symptom Tests########");
+		String rMsg = "";
+		launchBrowser(prop.getProperty("patient_url"));
+		MMPUtility mmpUtil = new MMPUtility(driver);
+		mmpUtil.login(prop.getProperty("patient_username"),prop.getProperty("patient_password"));
+		HomePage hPage = new HomePage(driver);
+		hPage.navigatetoAModule("Search Symptoms");
+		extentTest.info("Navigating to Schedule Appointment");
+		srchsymptomobj =  PageFactory.initElements(driver,SearchSymptomPage.class);
+		rMsg = srchsymptomobj.searchSymptom(srchsymptom);
+		ScreenshotUtil screenshotUtil = new ScreenshotUtil(driver);	
+		String screenshotPath = screenshotUtil.captureScreenshot("Valid_Search_Symptom");
+        extentTest.addScreenCaptureFromPath(screenshotPath,"Navigation_To_Valid_Search_Symptom");
+		SoftAssert sa = new SoftAssert();
+		sa.assertTrue(rMsg.isEmpty(), "Failed to search symptom");
+		screenshotUtil = new ScreenshotUtil(driver);
+		rMsg = srchsymptomobj.invalidsearchSymptom(invalidsrchsymptom);
+		screenshotPath = screenshotUtil.captureScreenshot("Invalid_Search_Symptom");
+        extentTest.addScreenCaptureFromPath(screenshotPath,"Navigation_To_Invalid_Search_Symptom");
+		sa.assertTrue(rMsg.isEmpty(), "Failed to search symptom");
+		
+	}
+
+}
