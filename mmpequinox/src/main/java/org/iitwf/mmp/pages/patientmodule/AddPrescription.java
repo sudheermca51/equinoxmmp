742cb1d95fe07b40c71810e92eab9ed5668695cf
santho17 <Santhoshi.Balasubramanian@gmail.com>
2025-03-20 19:17:12 -0400

    Add Prescription tests

---
+++ mmpequinox/src/main/java/org/iitwf/mmp/pages/patientmodule/AddPrescription.java
@@ -0,0 +1,99 @@
+package org.iitwf.mmp.pages.patientmodule;
+
+
+
+import java.util.HashMap;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class AddPrescription {
+	
+	WebDriver driver;
+	
+	HashMap<String,String> hMap = new HashMap<String,String>();
+	
+	By ssn = By.id("search");
+	By searchBtn = By.xpath("//input[@value='search']");
+	By nameLink = By.xpath("//div[@id='show']/table/tbody/tr/td/a");
+	By addPrescriptionBtn = By.xpath("//input[@value='Add Precription']");
+	By selectApptDropDown = By.id("app_date");
+	By prescriptionName = By.id("exampleInputcardnumber1");
+	By prescriptionDesc = By.name("p_desc");
+	By submitBtn = By.xpath("//input[@value='submit']");
+	By viewHistoryBtn = By.xpath("//button[text()='View History']");
+	By pastPrescriptionBtn = By.xpath("//input[@value='Past Prescription']");
+	By presMedicine = By.xpath("//ul[@class='list-inline list-users']/li");
+	
+	public AddPrescription(WebDriver driver) {
+		
+		this.driver = driver;
+	}
+	
+	public void enterSSN()
+	{
+
+		String ssnValue ="514236634";
+		driver.findElement(ssn).sendKeys(ssnValue);
+		hMap.put("SSN", ssnValue);
+	}
+	
+	public void search() {
+		
+		driver.findElement(searchBtn).click();
+	}
+	
+	public void name() {
+		
+		driver.findElement(nameLink).click();
+	}
+	
+	public void addPrescription() {
+		
+		driver.findElement(addPrescriptionBtn).click();
+	}
+	
+	public void selectAppointmentDate()
+	{
+	    new Select(driver.findElement(selectApptDropDown)).selectByVisibleText("04/09/2025");
+		hMap.put("SelectAppointmentDate", "04/09/2025");
+}
+	
+	public String prescriptionName(String prescription) {
+		
+		driver.findElement(prescriptionName).sendKeys(prescription);
+		return prescription;
+		
+	}
+	
+	public String prescriptionDescription(String description) {
+		
+		driver.findElement(prescriptionDesc).sendKeys(description);
+		return description;
+	}
+	
+	public void submit() {
+		
+		driver.findElement(submitBtn).click();
+	}
+	
+	public void viewHistory() {
+		
+		driver.findElement(viewHistoryBtn).click();
+	}
+	
+	public void pastPrescription() {
+		
+		driver.findElement(pastPrescriptionBtn).click();
+	}
+	
+	public String prescriptionMedicine() {
+		
+		WebElement presWE = driver.findElement(presMedicine);
+		String pres = presWE.getText();
+		System.out.println("Printing the Prescription: " +pres);
+		return pres;
+	}
+}
---
+++ mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/AddPrescriptionTests.java
@@ -0,0 +1,81 @@
+package org.iitwf.selenium.mmpequinox;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.iitwf.mmp.pages.patientmodule.AddPrescription;
+import org.iitwf.mmp.pages.patientmodule.HomePage;
+import org.iitwf.mmp.pages.patientmodule.MMPUtility;
+import org.iitwf.selenium.lib.FrameworkLibrary;
+import org.iitwf.selenium.lib.ScreenshotUtil;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+
+public class AddPrescriptionTests extends FrameworkLibrary {
+
+		AddPrescription prescription;
+		private ExtentTest extentTest;
+		
+		@Test
+		public void validatePrescription() throws IOException, InterruptedException
+		{
+			extentTest = extentReports.createTest("######## Prescription Test ########");
+						
+			launchBrowser(prop.getProperty("doc_url"));
+			MMPUtility mmpUtil = new MMPUtility(driver);
+			mmpUtil.docLogin(prop.getProperty("doc_username"),prop.getProperty("doc_password"));
+			prescription = new AddPrescription(driver);
+			//extentTest.info("Entering SSN number");
+			Thread.sleep(2000);
+			prescription.enterSSN();
+			prescription.search();
+			ScreenshotUtil screenshotUtil = new ScreenshotUtil(driver);	
+	        String screenshotPath = screenshotUtil.captureScreenshot("Patient_info");
+	        extentTest.addScreenCaptureFromPath(screenshotPath,"Displaying_Patient_info");
+			prescription.name();
+			prescription.addPrescription();
+			prescription.selectAppointmentDate();
+			String presName = prescription.prescriptionName("Diclofenac_0319");
+			extentTest.info("Entering Prescription Name -" + " " + presName);
+			String presDesc = prescription.prescriptionDescription("For Knee pain");
+			extentTest.info("Entering Prescription Description -" + " " + presDesc);
+			screenshotUtil = new ScreenshotUtil(driver);	
+	        screenshotPath = screenshotUtil.captureScreenshot("Prescription_info");
+	        extentTest.addScreenCaptureFromPath(screenshotPath,"Displaying_Prescription_info");
+	        prescription.submit();
+	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  // Wait for up to 10 seconds
+			Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+			Alert presAlert = driver.switchTo().alert();
+			String docactual = presAlert.getText();
+			extentTest.info("Getting Actual Alert Text - " + " " + docactual);
+			String docexpected ="Prescription has been Added.";
+			Assert.assertEquals(docactual, docexpected);
+			alert.accept();
+	    	extentTest.info("Launching Patient Url ");
+			launchBrowser(prop.getProperty("patient_url"));
+			mmpUtil.login(prop.getProperty("patient2_username"), prop.getProperty("patient2_password"));
+			HomePage hPage = new HomePage(driver);
+			hPage.navigatetoAModule("Profile");
+			prescription.viewHistory();
+			prescription.pastPrescription();
+			String actual = prescription.prescriptionMedicine();
+			String expected = "Diclofenac_0319";
+			extentTest.info("Prescription_information_for_Patient ");
+			screenshotUtil = new ScreenshotUtil(driver);	
+	        screenshotPath = screenshotUtil.captureScreenshot("Prescription info_in_patient");
+	        extentTest.addScreenCaptureFromPath(screenshotPath,"Displaying_Prescription_info_in_patient");
+			if(actual.contains(expected)) {
+				
+				System.out.println("Test case passed");
+			}
+			
+		}
+
+	
+
+}
