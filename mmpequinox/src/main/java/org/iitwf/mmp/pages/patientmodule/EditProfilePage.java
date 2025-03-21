6bb49a9256d334c915f0ae8e3aabd9db16a01ccd
srivisalii <srivisalii.com>
2025-03-21 11:41:34 -0400

    Upadate edit profile test cases.

--- mmpequinox/src/main/java/org/iitwf/mmp/pages/patientmodule/EditProfilePage.java
+++ mmpequinox/src/main/java/org/iitwf/mmp/pages/patientmodule/EditProfilePage.java
@@ -2,26 +2,29 @@ package org.iitwf.mmp.pages.patientmodule;
 
 import java.time.Duration;
 
+
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javaprograms.RandomEx;
 
 public class EditProfilePage {
 	
+	public String expectedName;
 	public String expectedFName;
-
-	@FindBy(id="Ebtn")
-	private WebElement editbutton;
+	//@FindBy(id="Ebtn")
+	//private WebElement editbutton;
+	/*private By editButton=By.id("Ebtn");
+	private By fname=By.xpath("(//input[@id='fname'])[1]");
+	private By age=By.xpath("(//input[@id='age'])[1]");
+	private By save=By.id("Sbtn");*/
+	private By actualFN=By.xpath("//tbody/tr[2]/td[1]");
 	
 	protected WebDriver driver;
 	public EditProfilePage(WebDriver driver)
@@ -31,107 +34,63 @@ public class EditProfilePage {
 			throw new IllegalStateException("This is not Profile Page," +
 					" current page is: " + driver.getCurrentUrl());
 		}
-		PageFactory.initElements(driver, this);
+		//PageFactory.initElements(driver, this);
 	}
 	public String editAllFields()
 	{
 		Actions action = new Actions(driver);
-
-		action.moveToElement(editbutton);
+		action.moveToElement(driver.findElement(By.id("Ebtn")));
 		action.click();
 
 		//Fname Logic
 		WebElement fnameWE = driver.findElement(By.id("fname"));
 		action.moveToElement(fnameWE);
-		action.sendKeys(Keys.CLEAR);
-		expectedFName = JavaUtility.generateRandomString("QAAUT");
-
-		System.out.println(expectedFName);
-		action.sendKeys(expectedFName);
-
-		action.sendKeys(expectedFName);
-		System.out.println("ExpectedFName :" + expectedFName);
-		
-		
-		
-		
-
-		
+		action.sendKeys(fnameWE,Keys.CLEAR);
+		String expectedFiName = JavaUtility.generateRandomString("QAAUT");
+		action.sendKeys(fnameWE,expectedFiName);
 		action.perform();
+		String actualFName = fnameWE.getDomProperty("value");
 		
-		
-
 		//Age Logic
 		WebElement ageWE = driver.findElement(By.id("age"));
 		action.moveToElement(ageWE);
-		action.sendKeys(Keys.CLEAR);
+		action.sendKeys(ageWE,Keys.CLEAR);
 		String ageExpected = JavaUtility.generateRandomDigits(30,99)+"";
-
-		String ageActual = ageWE.getDomProperty("value");
-		action.sendKeys(ageActual,ageExpected);
-
-		System.out.println("Expected Age :" + ageExpected);
-		action.sendKeys(ageExpected);
-		
-//		 String ageActual = ageWE.getDomProperty("value");
-//			action.sendKeys(ageActual,ageExpected);
-		
-
+		action.sendKeys(ageWE,ageExpected);
 		action.perform();
-
+		String ageActual = ageWE.getDomProperty("value");
 
 		WebElement saveButton = driver.findElement(By.id("Sbtn"));
 		action.moveToElement(saveButton);
 		action.click(saveButton);
 		action.perform();
+        
+		//WebDriverWait wait2 = new WebDriverWait(driver,Duration.ofSeconds(25));
+		//wait2.until(ExpectedConditions.domAttributeToBe(fnameWE, "value", expectedFName));
+		//JavascriptExecutor js = ((JavascriptExecutor)driver);
+		//js.executeScript("arguments[0].click();",fnameWE);
+		//WebDriverWait wait1 = new WebDriverWait(driver, Duration.ofSeconds(15));
+		//wait1.until(ExpectedConditions.domAttributeToBe(fnameWE, "value", expectedFName));
 		
-		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 		wait.until(ExpectedConditions.alertIsPresent());
 		
-	
-
-
 		Alert alrt = driver.switchTo().alert();
 		System.out.println("Alert Text " + alrt.getText());
 		alrt.accept();
 		
-		
-		WebDriverWait wait2 = new WebDriverWait(driver,Duration.ofSeconds(15));
-		wait2.until(ExpectedConditions.domAttributeToBe(fnameWE, "value", expectedFName));
-		
-		
-		JavascriptExecutor js = ((JavascriptExecutor)driver);
-		 js.executeScript("arguments[0].click();",fnameWE);
-		 
-		
-		
-
-		
+		/*WebDriverWait wait2 = new WebDriverWait(driver, Duration.ofSeconds(15));
+		wait.until(ExpectedConditions.domAttributeToBe(fnameWE, "value", expectedFName));
+			
+			 String actualFName = fnameWE.getDomProperty("value");
+				System.out.println(actualFName);
+			String actualFName = 	fnameWE.getDomAttribute("value");*/				
 	
-		  
-		  wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-			wait.until(ExpectedConditions.domAttributeToBe(fnameWE, "value", expectedFName));
-			
-//			 String actualFName = fnameWE.getDomProperty("value");
-//				System.out.println(actualFName);
-//				String actualFName = 	fnameWE.getDomAttribute("value");
-//				System.out.println(actualFName);
-			
-			String actualFName = fnameWE.getDomProperty("value");
-
-			System.out.println(actualFName);
-	  
-		  
-		  
-		  
-		 
-		 
-
-
-		return actualFName;
-
+    expectedName=driver.findElement(actualFN).getAccessibleName();
+	
+	return actualFName;
 	}
-	public  void editProfileTests()
+	/*public  void editProfileTests()
 	{
 		editbutton.click();
 		
@@ -159,7 +118,7 @@ public class EditProfilePage {
 		{
 			System.out.println("FName value is updated");
 		}
-	}
+	}*/
 	 
 		public  String handleAlerts()
 		{
@@ -169,6 +128,41 @@ public class EditProfilePage {
 			return alertTxt;
 		}
 		
-		
-	
+		public String invalidEditAllFields()
+		{
+			Actions action = new Actions(driver);
+			action.moveToElement(driver.findElement(By.id("Ebtn")));
+			action.click();
+
+			
+			WebElement fnameWEb = driver.findElement(By.id("fname"));
+			action.moveToElement(fnameWEb);
+			action.sendKeys(fnameWEb,Keys.CLEAR);
+			expectedFName = "abc1234";
+			action.sendKeys(fnameWEb,expectedFName);
+			action.perform();
+			String actualFName = fnameWEb.getDomProperty("value");
+			
+			WebElement saveButton = driver.findElement(By.id("Sbtn"));
+			action.moveToElement(saveButton);
+			action.click(saveButton);
+			action.perform();
+	        
+			
+			String actual = "";
+			if(expectedFName.equals(actualFName))
+			{
+				//System.out.println("please enter only alphabets");
+				WebElement nameWE = driver.findElement(By.xpath("(//p[@id='firsterr1'])[1]"));
+				actual= nameWE.getText();
+				
+			}
+			return actual;
+			
+			
+			
 }
+		
+}		
+		
+
--- mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/EditProfileTests.java
+++ mmpequinox/src/test/java/org/iitwf/selenium/mmpequinox/EditProfileTests.java
@@ -16,46 +16,75 @@ public class EditProfileTests extends FrameworkLibrary {
 
 	private ExtentTest extentTest;
 
-	@Test
+	@Test(priority=1)
 	public void validateEditFuncAllFields() throws IOException
 	{
 		extentTest = extentReports.createTest("############Validate Edit Profile Tests########");
-
-	    launchBrowser(prop.getProperty("patient_url"));
-	    
 		MMPUtility mmpUtil = new MMPUtility(driver);
-		mmpUtil.login(prop.getProperty("patient_username"),prop.getProperty("patient_password"));
+		extentTest.info("Launching Browser");
+		launchBrowser(prop.getProperty("patient_url"));
+	    mmpUtil.login(prop.getProperty("patient_username"),prop.getProperty("patient_password"));
 		HomePage hPage = new HomePage(driver);
-		hPage.navigatetoAModule("Profile");
+		hPage.navigateToAProfileModule("Profile");
 		extentTest.info("Navigating to Profile Page");
-		
-		
-		
 		
 		ScreenshotUtil screenshotUtil = new ScreenshotUtil(driver);	
 		String screenshotPath = screenshotUtil.captureScreenshot("EditProfilePage_step1");
 		extentTest.addScreenCaptureFromPath(screenshotPath,"Edit_Profile_Landing_Page");
 
-
+		extentTest.info("###################Validate Edit Profile Test#####################");
 		EditProfilePage editProjObj = new EditProfilePage(driver);
 		String actualFName = editProjObj.editAllFields();
 
-		String expectedFName=editProjObj.expectedFName;
+		String expectedFName=editProjObj.expectedName;
 
-		System.out.println(actualFName);
-//		String expectedFName=prop.getProperty("patient_username");
-		String expectedfName = editProjObj.expectedFName;
+		screenshotUtil = new ScreenshotUtil(driver);	
+		screenshotPath = screenshotUtil.captureScreenshot("EditProfilePage_step2");
+		extentTest.addScreenCaptureFromPath(screenshotPath,"Edit_Profile_Landing_Page");
+
+		System.out.println("ActualFName::"+ actualFName);
+		System.out.println("ExpectedFName::"+ expectedFName);
 		
-
 		extentTest.info("Expected FName::" + expectedFName);
 		extentTest.info("Actual FName::"+  actualFName );
 		
 		
-		Assert.assertEquals(actualFName, expectedfName);
+		Assert.assertEquals(actualFName, expectedFName);
 
 	}
+   @Test(priority=2)
+	public void invalidEditProfileTest() throws IOException 
 
+	{
+		extentTest = extentReports.createTest("############InValidate Edit Profile Tests########");
+		MMPUtility mmpUtil = new MMPUtility(driver);
+		extentTest.info("Launching Browser");
+	    launchBrowser(prop.getProperty("patient_url"));
+	    mmpUtil.login(prop.getProperty("patient_username"),prop.getProperty("patient_password"));
+		HomePage hPage = new HomePage(driver);
+		hPage.navigateToAProfileModule("Profile");
+		extentTest.info("Navigating to Profile Page");
+
+		ScreenshotUtil screenshotUtil = new ScreenshotUtil(driver);	
+		String screenshotPath = screenshotUtil.captureScreenshot("EditProfilePage_step3");
+		extentTest.addScreenCaptureFromPath(screenshotPath,"Edit_Profile_Landing_Page");
+
+		extentTest.info("####################InValidate Edit Profile Test#####################");
+		EditProfilePage editProjObj = new EditProfilePage(driver);
+		String errorMsg = editProjObj.invalidEditAllFields();
+
+		screenshotUtil = new ScreenshotUtil(driver);	
+		screenshotPath = screenshotUtil.captureScreenshot("EditProfilePage_step4");
+		extentTest.addScreenCaptureFromPath(screenshotPath,"Edit_Profile_Landing_Page");
+
+		System.out.println("Error Message is:"+ errorMsg );
+
+		extentTest.info("Error Message is::" +errorMsg );
+
+   }
+	
 }
+
 
 
 
